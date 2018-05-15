9e00676b4c4d0a177177c0c29bca7a76efa55b9e
Unknown <jjolula88@gmail.com>
2018-05-15 17:31:19 +0200

    15-05-Empresa

--- Proyecto-2DAM/app/src/main/java/com/example/jjsimon/proyectodam/PantallaUbicacionEquipo.java
+++ Proyecto-2DAM/app/src/main/java/com/example/jjsimon/proyectodam/PantallaUbicacionEquipo.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -14,10 +12,7 @@ import android.util.Log;
 
 import android.Manifest;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -29,15 +24,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-public class PantallaUbicacionEquipo extends FragmentActivity
-        implements OnMapReadyCallback,
-                    GoogleApiClient.ConnectionCallbacks,
-                    GoogleApiClient.OnConnectionFailedListener{
+public class PantallaUbicacionEquipo extends FragmentActivity implements OnMapReadyCallback{
 
     private static final int PETICION_PERMISO_LOCALIZACION = 101;
-    private GoogleApiClient apiClient;
-    private LocationRequest mLocationRequest;
-    private boolean mRequestingLocationUpdates;
+    private Location loc;
 
     private GoogleMap mMap;
 
@@ -48,7 +38,7 @@ public class PantallaUbicacionEquipo extends FragmentActivity
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-
+        pedirPermisos();
 
 
     }
@@ -71,111 +61,18 @@ public class PantallaUbicacionEquipo extends FragmentActivity
         mMap.moveCamera(CameraUpdateFactory.newLatLng(initLocation));
         mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
-        pruebaDani();
-        //obtenerPosicionActual();
-        //prueba();
+
 
 
     }
-
-
-   /* public void obtenerPosicionActual() {
-
-        Log.w("UBICACION", "Metodo llamado");
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-
-
-        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
-        double latitude = location.getLatitude();
-        double longitud = location.getLongitude();
-
-
-        Log.w("UBICACION", "Latitud "+latitude);
-        Log.w("UBICACION", "longitud "+longitud);
-    }
-*/
-
-
-
-    private void prueba(){
-        apiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addConnectionCallbacks(this)
-                .addApi(LocationServices.API)
-                .build();
-
-
-    }
-
-
-
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        //Conectado correctamente a Google Play Services
-        Log.d("LOCALIZACION", "onConnected");
-
-
-
-
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    PETICION_PERMISO_LOCALIZACION);
-        } else {
-
-            Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(apiClient);
-            Log.w("LOCALIZACION", ""+lastLocation);
-           // updateUI(lastLocation);
-           // double latitude = lastLocation.getLatitude();
-           // double longitud = lastLocation.getLongitude();
-
-
-
-
-            //Log.w("LOCALIZACION", "Latitud "+latitude);
-            //Log.w("LOCALIZACION", "longitud "+longitud);
-
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.d("LOCALIZACION", "onConnectionSuspended");
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        //Se ha producido un error que no se puede resolver automáticamente
-        //y la conexión con los Google Play Services no se ha establecido.
-        Log.d("LOCALIZACION", "onConnectionFailed");
-    }
-
 
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == PETICION_PERMISO_LOCALIZACION) {
-            if (grantResults.length == 1
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                //Permiso concedido
-
-                @SuppressWarnings("MissingPermission")
-                Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(apiClient);
-
-                Log.w("LOCALIZACION", ""+lastLocation);
-/*
-                double latitude = lastLocation.getLatitude();
-                double longitud = lastLocation.getLongitude();
-
-
-                Log.w("LOCALIZACION", "Latitud "+latitude);
-                Log.w("LOCALIZACION", "longitud "+longitud);
-*/
+                obtenerUbicacion();
             } else {
                 //Permiso denegado:
                 //Deberíamos deshabilitar toda la funcionalidad relativa a la localización.
@@ -188,66 +85,9 @@ public class PantallaUbicacionEquipo extends FragmentActivity
 
 
 
+    private Location obtenerUbicacion(){
+        loc = null;
 
-
-
-
-/*
-    private void initGooGleApiClient(){
-        if(mGoogleApiClient !=null){
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks) this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-
-
-        }
-    }
-
-
-
-    @SuppressLint("RestrictedApi")
-    private void createLocationRequest(){
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);//Crear constante para el tiempo
-        mLocationRequest.setFastestInterval(5000);//Crear constante para el tiempo
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-
-    }
-
-
-    private void startLocationUpdates(){
-        if(mGoogleApiClient!=null && mGoogleApiClient.isConnected() && !mRequestingLocationUpdates){
-            LocationServices.
-        }
-        mRequestingLocationUpdates = true;
-    }
-
-    private void stopLocationUpdates(){
-        mRequestingLocationUpdates = false;
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        Log.d("LOCALIZACION", "onConnected");
-        startLocationUpdates();
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.d("LOCALIZACION", "onConnectionSuspended");
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.d("LOCALIZACION", "onConnectionFailed");
-    }
-    */
-
-
-    private void pruebaDani(){
         FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(getApplicationContext());
 
         if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
@@ -267,6 +107,8 @@ public class PantallaUbicacionEquipo extends FragmentActivity
 
                         Log.w("LOCALIZACION", "Latitud "+latitude);
                         Log.w("LOCALIZACION", "longitud "+longitud);
+
+                        loc = location;
                     }
                 });
             } else {
@@ -274,5 +116,14 @@ public class PantallaUbicacionEquipo extends FragmentActivity
                 Log.w("PERMISOS", "No tiene permisos");
             }
         }
+
+        return loc;
+    }
+
+
+    public void pedirPermisos(){
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                PETICION_PERMISO_LOCALIZACION);
     }
 }
